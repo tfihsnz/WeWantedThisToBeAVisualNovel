22fc9c51914133e22fe30dc235b11c84fa4b9b53
tfihsnz <ljburrough@gmail.com>
2019-07-20 22:42:56 +1200

    created video and made it play

---
+++ DinoVillage/Assets/Scripts/PlayMenuVid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayMenuVid : MonoBehaviour
+{
+
+    public MovieTexture movie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+        //((MovieTexture)GetComponent<Renderer>().material.mainTexture).loop();
+        gameObject.GetComponent<RawImage>().texture = movie;
+        movie.Play();
+        movie.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
