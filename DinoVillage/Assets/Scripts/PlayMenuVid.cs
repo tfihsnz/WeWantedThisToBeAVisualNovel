787151bf7fe1436c31bd3d5764dacff2e0a64a53
tfihsnz <ljburrough@gmail.com>
2019-07-20 23:39:21 +1200

    added menus and got buttons working

--- DinoVillage/Assets/Scripts/PlayMenuVid.cs
+++ DinoVillage/Assets/Scripts/PlayMenuVid.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayMenuVid : MonoBehaviour
 {
-
+    public GameObject Canvas;
     public MovieTexture movie;
+    public GameObject MainUiCan;
 
     // Start is called before the first frame update
     void Start()
     {
+        MainUiCan.SetActive(false);
+        Time.timeScale = 0;
         //((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
         //((MovieTexture)GetComponent<Renderer>().material.mainTexture).loop();
         gameObject.GetComponent<RawImage>().texture = movie;
@@ -18,9 +22,16 @@ public class PlayMenuVid : MonoBehaviour
         movie.loop = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayGame()
     {
-        
+        Time.timeScale = 1;
+        Canvas.SetActive(false);
+        MainUiCan.SetActive(true);
+
+    }  
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
